0a1e00691dfac07a0bb3cea3fc4e81dc449e3dfd
Aman Hi <amanithetop@gmail.com>
2024-08-21 23:08:15 +0300

    install react and tailwind and create the first template

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2 className="underline font-bold text-red-500">hi aman</h2>
+    </>
+  );
+}
+
+export default App;
